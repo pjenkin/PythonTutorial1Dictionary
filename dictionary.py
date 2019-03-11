458979173c8dd2934cf65d807ee2b9ee4fcb4d2f
Peter N Jenkin <peternjenkin@gmail.com>
2019-03-11 10:48:47 +0000

    5-83 my way of handling invalid word enquiry

--- dictionary.py
+++ dictionary.py
@@ -5,9 +5,11 @@ def dictionary_fetch (key):
     """ this function d'fetch a dictionary definition"""
     # with json.load(open('UdemyDictionaryData.json')) as data:     # python did not like this
     #     return data[key]
-
-    data = json.load(open('UdemyDictionaryData.json'))
-    return data[key]
+    try:
+        data = json.load(open('UdemyDictionaryData.json'))              # could be from an API
+        return data[key]
+    except KeyError:
+        return 'No entry found for this word'
 
 
 # print(dictionary_fetch('rain'))
