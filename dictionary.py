2fc069f47f19331ff0f598b5edeb95bdc3a9456c
Peter N Jenkin <peternjenkin@gmail.com>
2019-03-11 10:51:58 +0000

    5-84 Lower case input to match dictionary keys' format

--- dictionary.py
+++ dictionary.py
@@ -5,9 +5,13 @@ def dictionary_fetch (key):
     """ this function d'fetch a dictionary definition"""
     # with json.load(open('UdemyDictionaryData.json')) as data:     # python did not like this
     #     return data[key]
+    data = json.load(open('UdemyDictionaryData.json'))  # could be from an API
+
+    key = key.lower()
+
     try:
-        data = json.load(open('UdemyDictionaryData.json'))              # could be from an API
-        return data[key]
+        if key in data:
+            return data[key]
     except KeyError:
         return 'No entry found for this word'
 
